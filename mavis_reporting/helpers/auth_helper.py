7d28ad6a5fc95a23697e5a4fd180daf17d504a73
Alistair Davidson <apdavidson@gmail.com>
2025-07-04 11:15:29 +0100

    add facility for FAKE_LOGIN_ENABLED, to allow local development without having to keep the main mavis running on the auth spike branch

--- mavis_reporting/helpers/auth_helper.py
+++ mavis_reporting/helpers/auth_helper.py
@@ -2,29 +2,62 @@ from datetime import datetime, timedelta, timezone
 
 from mavis_reporting.helpers import mavis_helper
 
+
 def session_expired(session, current_app):
-    last_visit = session.get('last_visit')
+    last_visit = session.get("last_visit")
     if last_visit is None:
         return True
-    
+
     session_age = (datetime.now().astimezone(timezone.utc) - last_visit).total_seconds()
-    return abs(session_age) > current_app.config['SESSION_TTL_SECONDS']
-    
+    return abs(session_age) > current_app.config["SESSION_TTL_SECONDS"]
+
+
 def is_logged_in(session, current_app):
-    if 'user_id' in session:
+    if "user_id" in session:
         if session_expired(session, current_app):
             session.clear()
             return False
         else:
             return True
-        
+
     else:
         return False
 
+
 def log_user_in(data, session):
-    session['user_id'] = data['user_id']
-    session['created_at'] = data['created_at']
-    session['last_visit'] = datetime.now()
-    session['cis2_info'] = data['cis2_info']
-    session['user'] = data['user']
-    
+    session["user_id"] = data["user_id"]
+    session["created_at"] = data["created_at"]
+    session["last_visit"] = datetime.now()
+    session["cis2_info"] = data["cis2_info"]
+    session["user"] = data["user"]
+
+
+def fake_login_enabled(current_app):
+    return bool(current_app.config.get("FAKE_LOGIN_ENABLED"))
+
+
+def fake_user_session_info():
+    return {
+        "user_id": 1,
+        "created_at": datetime.now() - timedelta(minutes=5),
+        "last_visit": datetime.now() - timedelta(minutes=1),
+        "user": {
+            "id": 1,
+            "email": "nurse.joy@example.com",
+            "created_at": "2025-06-16T11:09:24.289+01:00",
+            "updated_at": "2025-07-04T10:11:36.100+01:00",
+            "provider": None,
+            "uid": None,
+            "given_name": "Nurse",
+            "family_name": "Joy",
+            "session_token": None,
+            "fallback_role": "nurse",
+        },
+        "cis2_info": {
+            "selected_org": {"code": "R1L", "name": "SAIS Organisation 1"},
+            "selected_role": {
+                "code": "S8000:G8000:R8001",
+                "workgroups": ["schoolagedimmunisations"],
+            },
+        },
+    }

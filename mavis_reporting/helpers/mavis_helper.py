7812f4831270ea5188f81ff41063a3d859533aa5
Alistair Davidson <apdavidson@gmail.com>
2025-07-09 11:29:08 +0100

    Accept a JWT from the verify token endpoint, store it in session, and use it to authenticate Mavis API calls. Add an /api-call endpoint to show that it works

--- mavis_reporting/helpers/mavis_helper.py
+++ mavis_reporting/helpers/mavis_helper.py
@@ -3,13 +3,18 @@ import requests
 import urllib.parse
 
 
-def mavis_url(current_app, path):
-    return urllib.parse.urljoin(current_app.config["MAVIS_ROOT_URL"], path)
+def mavis_url(current_app, path, params={}):
+    url = urllib.parse.urljoin(current_app.config["MAVIS_ROOT_URL"], path)
+    if params != {}:
+        parsed_url = urllib.parse(url)
+        url_with_params = parsed_url._replace(query=params)
+        url = urllib.parse.urlunparse(url_with_params)
+
+    return url
 
 
-def verify_token(token, current_app):
+def verify_token(token, current_app, session):
     url = mavis_url(current_app, "/tokens/" + token)
-    user_data = None
     secret = current_app.config["SECRET_KEY"]
 
     headers = {"Authorization": secret}
@@ -17,5 +22,14 @@ def verify_token(token, current_app):
 
     token_data = r.json()
     jwt_data = jwt.decode(token_data["jwt"], secret, algorithms="HS512")
-
+    session["jwt"] = token_data["jwt"]
     return jwt_data["data"]
+
+
+def api_call(current_app, session, path, params={}):
+    url = mavis_url(current_app, path, params)
+    headers = {
+        "Authorization": "Bearer " + session["jwt"],
+        "Content-type": "application/json; charset=utf-8",
+    }
+    return requests.get(url, headers=headers)

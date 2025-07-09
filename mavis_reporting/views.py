369862267cb2e6fc4941ef1618c3f07f600f26e0
Alistair Davidson <apdavidson@gmail.com>
2025-07-09 11:29:08 +0100

    Accept a JWT from the verify token endpoint, store it in session, and use it to authenticate Mavis API calls. Add an /api-call endpoint to show that it works

--- mavis_reporting/views.py
+++ mavis_reporting/views.py
@@ -8,7 +8,6 @@ from flask import (
     url_for,
     current_app,
 )
-from functools import wraps
 
 import json
 import logging
@@ -16,37 +15,22 @@ import urllib.parse, urllib.request
 
 from mavis_reporting.helpers import mavis_helper
 from mavis_reporting.helpers import auth_helper
+from mavis_reporting.helpers import url_helper
 
 logger = logging.getLogger(__name__)
 
 main = Blueprint("main", __name__)
 
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if auth_helper.fake_login_enabled(current_app):
-            logger.warning("FAKE_LOGIN_ENABLED! Logging in as fake_user_session_info")
-            auth_helper.log_user_in(auth_helper.fake_user_session_info(), session)
-
-        elif not auth_helper.is_logged_in(session, current_app):
-            token = request.args.get("token")
-            if token:
-                user_data = mavis_helper.verify_token(token, current_app)
-                auth_helper.log_user_in(user_data, session)
-            else:
-                target_url = mavis_helper.mavis_url(
-                    current_app,
-                    "/start?redirect_after_login=" + urllib.parse.quote(request.url),
-                )
-                return redirect(target_url)
-
-        return f(*args, **kwargs)
-
-    return decorated_function
-
-
 @main.route("/")
-@login_required
+@auth_helper.login_required
 def index():
     return render_template("index.jinja")
+
+
+@main.route("/api-call")
+@auth_helper.login_required
+def api_call():
+    response = mavis_helper.api_call(current_app, session, "/reporting/totals")
+    data = response.json()
+    return render_template("api_call.jinja", response=response, data=data)

bcb0bab37569bec34a947e0ead7857b055d81f29
Mike Thompson <mike@artifactual.co.uk>
2025-07-11 17:17:59 +0100

    Add simple dashboard pages with static data

--- mavis_reporting/views.py
+++ mavis_reporting/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 import logging
 
+from mavis_reporting.config.mavis import programmes, year_groups, genders
+
 logger = logging.getLogger(__name__)
 
 main = Blueprint("main", __name__)
@@ -8,4 +10,21 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    return render_template("index.jinja")
+    return render_template(
+        "index.jinja",
+        page="region",
+        programmes=programmes,
+        year_groups=year_groups,
+        genders=genders,
+    )
+
+
+@main.route("/providers")
+def providers():
+    return render_template(
+        "providers.jinja",
+        page="providers",
+        programmes=programmes,
+        year_groups=year_groups,
+        genders=genders,
+    )
